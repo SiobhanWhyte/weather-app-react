cc9701b981caffc7adf44338f33f69b2c67b2ce0
SiobhanWhyte <77633144+SiobhanWhyte@users.noreply.github.com>
2021-03-11 11:57:06 +0000

    Edit to bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Weather from "./Weather.js";
+import 'bootstrap/dist/css/bootstrap.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 
 import "./App.css";
 
